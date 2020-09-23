2371d51295c5871de4c5901b7db6cd3f1e2e1041
Tycho Bellers <tycho.bellers999@gmail.com>
2020-09-22 19:37:18 -0700

    bug fix again

--- keras_retinanet/bin/train.py
+++ keras_retinanet/bin/train.py
@@ -457,8 +457,6 @@ def parse_args(args):
 
 def main(args=None):
     print('CALLED MAIN WITH ARGS:', args)
-    sys.stdout.write('THIS IS STDOUT')
-    sys.stderr.write('THIS IS STDERR')
 
     # parse arguments
     if args is None:
